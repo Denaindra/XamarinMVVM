763b376247fb2f8c9f736e907e4eff99a6fe13e0
Gayan Denaindra Perera <gadperera@cmdgadperera.local>
2017-02-24 12:32:24 +0530

    MVVM clock app

---
+++ MVVM_1/ClockViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MVVM_1
+{
+	public class ClockViewModel : INotifyPropertyChanged
+	{
+		private DateTime dateTime;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public ClockViewModel()
+		{
+			this.dateTime = DateTime.Now;
+			Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+			{
+				this.dateTime = DateTime.Now;
+				return true;
+			});
+		}
+
+		public DateTime DateTime
+		{
+			get
+			{
+				return dateTime;
+			}
+			set
+			{
+				if (dateTime != value)
+				{
+					dateTime = value;
+
+					if (PropertyChanged != null)
+					{
+						PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
+					}
+				}
+			}
+		}
+
+	}
+}
---
+++ MVVM_1/MVVM_1Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MVVM_1
+{
+	public partial class MVVM_1Page : ContentPage
+	{
+		public MVVM_1Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
